13cbdfe720ba0ffeff86f7a0bd3ee3cca0facde6
Ryan Fiset <ryfiset@gmail.com>
2023-07-04 18:51:07 -0700

    Add python aliases for fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,9 @@ starship init fish | source
 # Alias for nvim
 alias v="nvim"
 
+# Aliases for pip and python to remove 3 at the end
+alias python="python3"
+alias pip="pip3"
+
 # Setting PATH for Python 3.11
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
