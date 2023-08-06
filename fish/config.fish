7ec1b524b93f7b06cc48e37b92e4e30fd3b2bf46
Ryan Fiset <ryfiset@gmail.com>
2023-08-06 10:36:24 -0700

    Update fish configs and starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-# Set prompt
-starship init fish | source
 
 # Alias for nvim
-alias v="nvim"
+alias v="vim"
 
 # Aliases for pip and python to remove 3 at the end
 alias python="python3"
@@ -13,3 +11,6 @@ alias pip="pip3"
 
 # Setting PATH for Python 3.11
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
+
+# Set prompt
+starship init fish | source
