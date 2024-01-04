743a7135a6bc299d627756418fd5e640fd41dfce
Ryan Fiset <ryfiset@gmail.com>
2024-01-04 11:28:59 -0800

    Update rust lsp

--- nvim/lua/ryan/plugins/lsp/rust.lua
+++ nvim/lua/ryan/plugins/lsp/rust.lua
@@ -1,11 +1,26 @@
 return {
 	"mrcjkb/rustaceanvim",
+	event = "BufReadPost",
 	version = "^3", -- Recommended
 	ft = { "rust" },
 	config = function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		vim.keymap.set("n", "<leader>a", function()
-			vim.cmd.RustLsp("codeAction")
-		end, { silent = true, buffer = bufnr })
+		vim.g.rustaceanvim = {
+			-- Plugin configuration
+			-- tools = {},
+			-- LSP configuration
+			server = {
+				on_attach = function(client, bufnr)
+					-- you can also put keymaps in here
+					vim.lsp.inlay_hint(bufnr, true)
+				end,
+				settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {},
+				},
+				--   },
+				-- DAP configuration
+				-- dap = {},
+			},
+		}
 	end,
 }
