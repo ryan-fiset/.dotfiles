e687577827e56c6d6147beaf06ce2a5bd9bf80c8
Ryan Fiset <ryfiset@gmail.com>
2024-08-11 12:52:15 -0700

    Remove unneeded plugins

--- nvim/lua/ryan/plugins/helpers.lua
+++ nvim/lua/ryan/plugins/helpers.lua
@@ -29,36 +29,11 @@ return {
 			},
 		},
 	},
-	-- { -- See keybinds
-	-- 	"folke/which-key.nvim",
-	-- 	event = "VimEnter",
-	-- 	config = function()
-	-- 		require("which-key").setup()
-	--
-	-- 		-- Document existing key chains
-	-- 		require("which-key").register({
-	-- 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-	-- 			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-	-- 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-	-- 			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-	-- 			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-	-- 			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-	-- 			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-	-- 		})
-	-- 		-- visual mode
-	-- 		require("which-key").register({
-	-- 			["<leader>h"] = { "Git [H]unk" },
-	-- 		}, { mode = "v" })
-	-- 	end,
-	-- },
 	{ -- Add highlighting to comments starting with TODO, NOTE, etc
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
-	},
-	{ -- Navigate between nvim and wezterm multiplexer
-		"mrjones2014/smart-splits.nvim",
 	},
 	{ -- Autopair brackets and quotes
 		"windwp/nvim-autopairs",
